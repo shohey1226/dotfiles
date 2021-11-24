6e7e897ed0e1ea465436d6b3bbe3d683dadc775d
shohey1226 <shoheik@cpan.org>
2021-11-24 03:16:02 +0000

    moved update to top

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+sudo apt-get -y update
+
 # Neovim
 sudo apt-get -y install neovim 
-sudo apt-get -y install python-neovim
-sudo apt-get -y install python3-neovim
 
 # dein
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
@@ -14,12 +14,11 @@ sudo apt-get -y install fzf
 
 # nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+nvm install --lts
 
 # rbenv
 sudo apt-get -y install rbenv
 
 # fish shell
 sudo apt-add-repository ppa:fish-shell/release-3
-sudo apt-get -y update
 sudo apt-get -y install fish
-
